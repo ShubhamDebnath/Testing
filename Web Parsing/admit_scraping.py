ec00d9954a9b377dca4710a8eb4506ce412ca50a
ShubhamDebnath <gamefree89@rocketmail.com>
2018-05-05 19:00:30 +0530

    Update and rename admit scraping.py to admit_scraping.py

--- Web Parsing/admit_scraping.py
+++ Web Parsing/admit_scraping.py
@@ -5,8 +5,8 @@ import os
 url = 'http://HIDDEN IP ADDRESS OF SERVER/jasperserver/flow.html?rollno={0}&_flowId=viewReportFlow&reportUnit=%2Freports%2FMAKAUT%2FPRE_REPORT%2FAdmit_Card&standAlone=true&ParentFolderUri=/reports/MAKAUT/PRE_REPORT&j_username=LSPL&j_password=lspl12345&decorate=no&output=pdf&semcode=SM{1:2f}'
 file_name = '{0}/Admit Card {1}.pdf'
 baseRollNo = {'Computer Science 6th Sem' : XXXXXXXXXXX,      # add more roll numbers to this list, keep last 2 digits 00
-                'Electrical 6th Sem' : XXXXXXXXXXX 
-                'Civil 6th Sem' : XXXXXXXXXXX 
+                'Electrical 6th Sem' : XXXXXXXXXXX ,
+                'Civil 6th Sem' : XXXXXXXXXXX ,
                 'ECE 6th Sem' : XXXXXXXXXXX
                 }                                           
 
