d03ea4930e9931538887689edc169d60c87af0a8
ShubhamDebnath <gamefree89@rocketmail.com>
2018-05-09 22:10:46 +0530

    Update art_generation.py

--- Neural Style Transfer/art_generation.py
+++ Neural Style Transfer/art_generation.py
@@ -19,12 +19,12 @@ STYLE_LAYERS  = [
     ('conv5_1', 0.2)]
 
 style_list = ["composition", "monet", "picasso", "seated_nude", "stone", "van_gogh"]
-video_name = "10seconds"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--all", help="run for some predefined style images")
 parser.add_argument("--style", default = "composition", help="put name of style image you want to use without extension " )
 parser.add_argument("--content", default = "louvre_small", help="put name of content image you want to use without extension " )
+parser.add_argument("--video", default = "10seconds", help="put name of video to render")
 args = parser.parse_args()
 
 
@@ -82,7 +82,7 @@ def total_cost(J_content, J_style, alpha = 10, beta = 40):
     
     return J
 
-def model_nn(model, sess, input_image, train_step, style_string, content_string, num_iterations = 200):
+def model_nn(model, sess, input_image, train_step, style_string, content_string = None, num_iterations = 200, video = False):
     
     sess.run(tf.global_variables_initializer())
     sess.run(model["input"].assign(input_image))
@@ -93,7 +93,7 @@ def model_nn(model, sess, input_image, train_step, style_string, content_string,
 
         generated_image = sess.run(model["input"])
 
-        if i%20 == 0:
+        if i%20 == 0 and not video:
             # Jt, Jc, Js = sess.run([J, J_content, J_style])
             # print("Iteration " + str(i) + " :")
             # print("total cost = " + str(Jt))
@@ -101,70 +101,71 @@ def model_nn(model, sess, input_image, train_step, style_string, content_string,
             # print("style cost = " + str(Js))
             
             save_image("output/{0}/{0}_{1}_{2}.png".format(style_string, content_string, str(i)), generated_image)
-    
-    save_image('output/{0}/generated_image_{0}_{1}.jpg'.format(style_string, content_string), generated_image)
+    if not video:
+        save_image('output/{0}/generated_image_{0}_{1}.jpg'.format(style_string, content_string), generated_image)
     
     return generated_image
 
-# def on_video():
+def on_video():
 
-#     style_image = scipy.misc.imread("images/style_{}.jpg".format(style_string))
-#     style_image = reshape_and_normalize_image(style_image)
+    style_image = scipy.misc.imread("images/style_{}.jpg".format(style_string))
+    style_image = reshape_and_normalize_image(style_image)
 
 
-#     if not os.path.exists(os.path.join("output/", style_string)):
-#         os.makedirs(os.path.join("output/", style_string))
+    if not os.path.exists(os.path.join("output/", style_string)):
+        os.makedirs(os.path.join("output/", style_string))
 
-#     cap = cv2.VideoCapture("videos/{}.mp4".format(video_name))
-#     print("video opened")
-#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#     out_string = "output/{0}/{0}_{1}.avi".format(style_string, video_name)
-#     output = cv2.VideoWriter(out_string, fourcc, 20.0, (400, 300))
+    cap = cv2.VideoCapture("videos/{}.mp4".format(video_string))
+    print("video opened")
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out_string = "output/{0}/{0}_{1}.avi".format(style_string, video_string)
+    output = cv2.VideoWriter(out_string, fourcc, 20.0, (400, 300))
 
-#     while(cap.isOpened()):
-#         ret, frame = cap.read()
+    while(cap.isOpened()):
+        ret, frame = cap.read()
 
-#         if ret == True:
-#             tf.reset_default_graph()
-#             sess = tf.InteractiveSession()
-#             model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
+        if ret == True:
+            tf.reset_default_graph()
+            sess = tf.InteractiveSession()
+            model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
 
-#             frame = cv2.resize(frame, (400, 300))
-#             content_image = reshape_and_normalize_image(frame)
-#             generated_image = generate_noise_image(content_image)
+            frame = cv2.resize(frame, (400, 300))
+            content_image = reshape_and_normalize_image(frame)
+            generated_image = generate_noise_image(content_image)
 
-#             sess.run(model['input'].assign(content_image))
+            sess.run(model['input'].assign(content_image))
 
-#             out = model['conv4_2']
-#             a_C = sess.run(out)
-#             a_G = out
+            out = model['conv4_2']
+            a_C = sess.run(out)
+            a_G = out
 
-#             J_content = compute_content_cost(a_C, a_G)
+            J_content = compute_content_cost(a_C, a_G)
 
-#             sess.run(model['input'].assign(style_image))
-#             J_style = compute_style_cost(model, sess, STYLE_LAYERS)
+            sess.run(model['input'].assign(style_image))
+            J_style = compute_style_cost(model, sess, STYLE_LAYERS)
             
-#             J = total_cost(J_content, J_style, alpha = 10, beta = 80)
+            J = total_cost(J_content, J_style, alpha = 10, beta = 80)
 
-#             optimizer = tf.train.AdamOptimizer(2.0)
-#             train_step = optimizer.minimize(J)
+            optimizer = tf.train.AdamOptimizer(2.0)
+            train_step = optimizer.minimize(J)
 
-#             generated_image = model_nn(model, sess, generated_image, train_step, style_string, content_string, num_iterations = 200)
+            generated_image = model_nn(model, sess, generated_image, train_step, style_string, content_string, num_iterations = 200, video = True)
 
-#             # Un-normalize the image so that it looks good
-#             generated_image = generated_image + CONFIG.MEANS
+            # Un-normalize the image so that it looks good
+            generated_image = generated_image + CONFIG.MEANS
     
-#             # Clip and Save the image
-#             generated_image = np.clip(generated_image[0], 0, 255).astype('uint8')
+            # Clip and Save the image
+            generated_image = np.clip(generated_image[0], 0, 255).astype('uint8')
 
-#             output.write(generated_image)
-#             print("*", end = "")
-#         else:
-#             break
+            output.write(generated_image)
+            print("*", end = "")
+            sess.close()
+        else:
+            break
 
-#     print("video saved")
-#     cap.release()
-#     output.release()
+    print("video saved")
+    cap.release()
+    output.release()
 
 def on_image(style_string, content_string):
     tf.reset_default_graph()
@@ -212,6 +213,11 @@ if __name__ == '__main__':
     if args.all:
         for style_string in style_list:
             on_image(style_string, args.content)
+            
+    elif args.video:
+        style_string = args.style
+        video_string = args.video
+        on_video(style_string, video_string)
 
     else:
         style_string = args.style
